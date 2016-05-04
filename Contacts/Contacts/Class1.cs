612a8a861713658e45a12e6aee48fd64db65ffda
Eric Gottschalk <ericng04@hotmail.com>
2016-05-04 14:13:41 -0300

    AWESOME CHANGES 2.0

---
+++ Contacts/Contacts/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts
+{
+    public class Class1
+    {
+        public List<Contact> CONTATOS { get; set; }
+        public Contact Get() => null;
+
+        public Contact GET_PORID(int id) => default(Contact);
+
+        public IEnumerable<Contact> Listar_TODOS() => default(IEnumerable<Contact>);
+
+        public void ADICIONAR_CONTACT(object contact)
+        {
+            if (contact is Contact)
+            {
+                var contactContact = (Contact)contact;
+                CONTATOS.Add(contactContact);
+            }
+
+            throw new Exception("CONTACT IS NOT A CONTACT");
+        }
+
+        public Class1()
+        {
+            CONTATOS = new List<Contact>();
+        }
+    }
+}
